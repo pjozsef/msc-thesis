5bcd908fa1b1cca9d682337becbb9ae923ec8af5
jpollak <jozsef.pollak@emarsys.com>
2017-09-30 20:02:38 +0200

    feat(conversion-utils): normalize file when converting to wav

--- dsp/src/main/kotlin/com/github/pjozsef/thesis/dsp/utils/ConversionUtils.kt
+++ dsp/src/main/kotlin/com/github/pjozsef/thesis/dsp/utils/ConversionUtils.kt
@@ -2,11 +2,17 @@ package com.github.pjozsef.thesis.dsp.utils
 
 import java.io.File
 
-fun convertToWav(path: String){
+fun convertToWav(path: String) {
     val file = File(path).absoluteFile
     val fileName = file.nameWithoutExtension
     val folder = file.parent
     val sep = File.separator
     val outputPath = "$folder$sep$fileName.wav"
-    execute("ffmpeg", "-i", file.absolutePath, "-ac", "1", "-ar", "44100", "-y", outputPath)
+    execute("ffmpeg",
+            "-i", file.absolutePath,
+            "-ac", "1",
+            "-ar", "44100",
+            "-filter:a", "loudnorm",
+            "-y",
+            outputPath)
 }
