af28fd13fa43e638d8004be1d4e95fb4122c60f2
jpollak <jozsef.pollak@emarsys.com>
2018-03-02 00:40:18 +0100

    feat(model): add inference + tsnee dimensionality reduction

---
+++ model/src/infer/infer.py
@@ -0,0 +1,52 @@
+import argparse
+import csv
+import glob
+
+import matplotlib.image as mpimg
+import numpy as np
+import tensorflow as tf
+
+
+def extract_info(path, info_labels, encoded, encoded_dimensions):
+    file_name = path.split("/")[-1][:-4]
+    parts = file_name.split("__")
+    result = {}
+    for info, label in zip(parts, info_labels):
+        result[label] = info
+    for encoding, label in zip(encoded, encoded_dimensions):
+        result[label] = encoding
+    return result
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-glob', required=True)
+parser.add_argument('--model', required=True)
+parser.add_argument('--export', required=True)
+parser.add_argument('--verbose', action='store_true')
+args = parser.parse_args()
+
+if args.verbose:
+    for path in glob.glob(args.data_glob):
+        print(path)
+
+glob_input = glob.glob(args.data_glob)
+paths = [p for p in glob_input]
+images = [mpimg.imread(p) for p in glob_input]
+
+info_labels = ['style', 'artist', 'album', 'song', 'percentile']
+encoded_dimensions = [str(i) for i in range(33)]
+fieldnames = info_labels + encoded_dimensions
+
+with tf.Session(graph=tf.Graph()) as sess:
+    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], args.model)
+    encoded = sess.graph.get_tensor_by_name("fc5/encoded:0")
+
+    with open(args.export, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for path, image in zip(paths, images):
+            image = np.array(image).reshape([-1, 800, 20, 1])
+            codes = np.reshape(sess.run(encoded, feed_dict={'x:0': image}), [32])
+            writer.writerow(extract_info(path, info_labels, codes, encoded_dimensions))
+            print("Finished", path)
---
+++ model/src/infer/tsne.py
@@ -0,0 +1,45 @@
+import csv
+import time
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+header = None
+infos = []
+codes = []
+with open('ultramini.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)
+    for row in reader:
+        if row[4] == '75':
+            infos.append(row[0:5])
+            codes.append(row[5:37])
+
+start = time.time()
+tsne = TSNE(learning_rate=10)
+codes_2d = tsne.fit_transform(codes)
+print("Iterations:", tsne.n_iter)
+print("Iterations:", tsne.n_iter_)
+print("Divergence:", tsne.kl_divergence_)
+print("T-SNE finished in", time.time() - start, "seconds")
+
+x = codes_2d[:, 0]
+y = codes_2d[:, 1]
+label = []
+labelMapping = {
+    'classical': 0,
+    'electronic': 1,
+    'metal': 2
+}
+for info in infos:
+    label.append(labelMapping[info[0]])
+
+colorMapping = {
+    0: 'blue',
+    1: 'orange',
+    2: 'black'
+}
+colors = [colorMapping[style] for style in label]
+fig = plt.figure(figsize=(8, 8))
+plt.scatter(x, y, c=colors)
+plt.show()
