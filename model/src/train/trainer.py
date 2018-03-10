14befe8b8c97838e4a69473b74c07a651256118c
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-03-10 20:46:34 +0100

    feat(trainer): add separate train/cv/test sections

--- model/src/train/trainer.py
+++ model/src/train/trainer.py
@@ -6,28 +6,40 @@ import numpy as np
 import tensorflow as tf
 
 from src.train import proto_parser
-from src.train.model import model02
+from src.train.model import model01
 
 if __name__ == "__main__":
     print("Arguments", sys.argv)
     parser = argparse.ArgumentParser()
     parser.add_argument('--train-data-root', required=True)
     parser.add_argument('--train-data-records', nargs='*', required=True)
+    parser.add_argument('--cv-data-root', required=True)
+    parser.add_argument('--cv-data-records', nargs='*', required=True)
+    parser.add_argument('--test-data-root', required=True)
+    parser.add_argument('--test-data-records', nargs='*', required=True)
     parser.add_argument('--job-dir', required=True)
     parser.add_argument('--take', type=int)
     args = parser.parse_args()
     print("Parsed arguments:", args)
 
-    input_records = [args.train_data_root + "/" + record for record in args.train_data_records]
-    print(input_records)
+    train_records = [args.train_data_root + "/" + record for record in args.train_data_records]
+    print("Train records:", train_records)
+
+    cv_records = [args.cv_data_root + "/" + record for record in args.cv_data_records]
+    print("CV records:", cv_records)
+
+    test_records = [args.test_data_root + "/" + record for record in args.test_data_records]
+    print("Test records:", test_records)
 
     MODEL_NAME = "model-simple"
     BATCH_SIZE = 256
-    PREFETCH_BUFFER = 1000
-    SHUFFLE_BUFFER = 1000
+    PREFETCH_BUFFER = 2500
+    SHUFFLE_BUFFER = 2500
     TAKE = args.take
-    EPOCH = 40
+    EPOCH = 10
     LEARNING_RATE = 0.001
+    CONV_KEEP_PROB = 0.95
+    FC_KEEP_PROB = 0.6
     print("Batch size:", BATCH_SIZE)
     print("Prefetch buffer:", PREFETCH_BUFFER)
     print("Shuffle buffer:", SHUFFLE_BUFFER)
@@ -47,7 +59,7 @@ if __name__ == "__main__":
 
     iterator = dataset.make_initializable_iterator()
 
-    x, encoded, y, conv_keep_prob, fc_keep_prob = model02.create_model(tf.nn.relu, tf.nn.relu)
+    x, encoded, y, conv_keep_prob, fc_keep_prob = model01.create_model(tf.nn.elu, tf.nn.elu)
     print("Model created")
 
     cost = tf.reduce_sum(tf.square(y - x))
@@ -56,8 +68,8 @@ if __name__ == "__main__":
     init_op = tf.group(tf.global_variables_initializer(),
                        tf.local_variables_initializer())
 
-    tf.summary.image("summary_x", x, max_outputs=3)
-    tf.summary.image("summary_y", y, max_outputs=3)
+    tf.summary.image("summary_x", x, max_outputs=1)
+    tf.summary.image("summary_y", y, max_outputs=1)
     tf.summary.scalar("summary_cost", cost)
     tf.summary.histogram("summary_cost_hist", cost)
 
@@ -90,13 +102,15 @@ if __name__ == "__main__":
 
         summary = None
         step = 0
+        #######################
+        #        TRAIN        #
+        #######################
         for epoch in range(EPOCH + 1):
             print("Epoch:", epoch)
             epoch_start = time.time()
-            sess.run(iterator.initializer, feed_dict={filenames: input_records})
+            sess.run(iterator.initializer, feed_dict={filenames: train_records})
             summary = tf.summary.merge_all()
-            total_batch = 0
-            total_error = 0
+
             times_for_mini_batch = []
             times_for_optimizer = []
             while True:
@@ -105,28 +119,56 @@ if __name__ == "__main__":
                     images, dimensions, names = sess.run(next_element)
                     times_for_mini_batch.append(time.time() - start)
 
-                    feed_dict = {x: images, conv_keep_prob: 0.95, fc_keep_prob: 0.6}
-
                     start = time.time()
-                    if epoch % 5 == 0:
-                        _, cost_value = sess.run([optimizer, cost], feed_dict)
-                        total_error += cost_value
-                        total_batch += images.shape[0]
-                        if images.shape[0] == BATCH_SIZE:
-                            all_summary = sess.run(summary, feed_dict)
-                            writer.add_summary(all_summary, step)
-                    else:
-                        sess.run(optimizer, feed_dict)
-
-                    if images.shape[0] == BATCH_SIZE:
-                        step += 1
+                    sess.run(
+                        optimizer,
+                        {x: images, conv_keep_prob: CONV_KEEP_PROB, fc_keep_prob: FC_KEEP_PROB})
                     times_for_optimizer.append(time.time() - start)
                 except tf.errors.OutOfRangeError:
-                    if total_error:
-                        print("Mean error for epoch", epoch, ":", (total_error / total_batch))
                     print("Total epoch time:", (time.time() - epoch_start) / 60, "minutes")
                     print("Average times for minibatch: ", np.average(times_for_mini_batch))
                     print("Average times for calculation: ", np.average(times_for_optimizer))
+
+                    ########################
+                    #          CV          #
+                    ########################
+                    if epoch % 5 == 0:
+                        print("Running CV")
+                        total_batch = 0
+                        total_error = 0.0
+                        sess.run(iterator.initializer, feed_dict={filenames: cv_records})
+                        while True:
+                            try:
+                                images, dimensions, names = sess.run(next_element)
+                                if images.shape[0] == BATCH_SIZE:
+                                    cost_cv, all_summary = sess.run(
+                                        [cost, summary],
+                                        {x: images, conv_keep_prob: 1.0, fc_keep_prob: 1.0})
+                                    writer.add_summary(all_summary, step)
+                                    total_batch += 1
+                                    total_error += cost_cv
+                                    step += 1
+                            except tf.errors.OutOfRangeError:
+                                print("Mean CV error for epoch", epoch, ":", (total_error / total_batch))
+                                break
                     break
             saver.save(sess, args.job_dir + '/' + MODEL_NAME + '.ckpt', global_step=epoch)
         savedModelBuilder.save()
+
+        ########################
+        #         TEST         #
+        ########################
+        print("Running TEST")
+        total_batch = 0
+        total_error = 0.0
+        sess.run(iterator.initializer, feed_dict={filenames: test_records})
+        while True:
+            try:
+                images, dimensions, names = sess.run(next_element)
+                cost_test = sess.run(cost, {x: images, conv_keep_prob: 1.0, fc_keep_prob: 1.0})
+                total_batch += 1
+                total_error += cost_test
+                step += 1
+            except tf.errors.OutOfRangeError:
+                print("Final TEST error", ":", (total_error / total_batch))
+                break
