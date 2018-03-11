8e7b5ed2e484420e2c6913a6620608277523ae5f
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-03-11 21:47:52 +0100

    feat(infer): implement knn on encoded dataset

---
+++ model/src/infer/neighbours.py
@@ -0,0 +1,29 @@
+import argparse
+
+import numpy as np
+import sklearn.neighbors as neighbors
+
+import src.infer.csv_parser as csv_parser
+import src.infer.percentile_parser as percentile_parser
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', required=True)
+parser.add_argument('--topk', type=int, default=3)
+parser.add_argument('--percentiles')
+args = parser.parse_args()
+if args.percentiles:
+    args.percentiles = percentile_parser.parse_percentiles(args.percentiles)
+print(args)
+
+codes, infos = csv_parser.parse(args.input, args.percentiles)
+tree_kd = neighbors.KDTree(codes, leaf_size=32)
+low = 0
+high = len(codes) - 1
+random_index = np.random.randint(0, high)
+distances, indices = tree_kd.query([codes[random_index]], k=args.topk)
+print(indices)
+print(distances)
+for i in indices[0]:
+    print(i)
+    t = tuple(codes[i])
+    print(infos[i])
