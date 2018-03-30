6691c8b64b781433ababa6eccbff3ed9526f1d39
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-03-30 20:56:39 +0200

    feat(model): output neighbour result with tabulate

--- model/src/infer/neighbours.py
+++ model/src/infer/neighbours.py
@@ -2,6 +2,7 @@ import argparse
 
 import numpy as np
 import sklearn.neighbors as neighbors
+from tabulate import tabulate
 
 import src.infer.csv_parser as csv_parser
 import src.infer.percentile_parser as percentile_parser
@@ -14,6 +15,7 @@ args = parser.parse_args()
 if args.percentiles:
     args.percentiles = percentile_parser.parse_percentiles(args.percentiles)
 print(args)
+print()
 args.topk = args.topk + 1
 
 codes, infos = csv_parser.parse(args.input, args.percentiles)
@@ -23,16 +25,26 @@ high = len(codes) - 1
 random_index = np.random.randint(0, high)
 distances, indices = tree_kd.query([codes[random_index]], k=args.topk)
 
+values = []
 for i, index in enumerate(indices[0]):
-    if i == 0:
-        print("Starting point")
-    else:
-        print("{}. neighbour".format(i))
-        print("Distance", distances[0][i])
-    print("Index:", index)
-    print("Style:", infos[index]['style'])
-    print("Artist:", infos[index]['artist'])
-    print("Album:", infos[index]['album'])
-    print("Song:", infos[index]['song'])
-    print("Percentile:", infos[index]['percentile'])
-    print()
+    values.append([
+        i,
+        index,
+        distances[0][i],
+        infos[index]['style'],
+        infos[index]['artist'],
+        infos[index]['album'],
+        infos[index]['song'],
+        infos[index]['percentile'],
+    ])
+
+print(tabulate(values,
+               headers=[
+                   'Row',
+                   'Index',
+                   'Distance',
+                   'Style',
+                   'Artist',
+                   'Album',
+                   'Song',
+                   'Percentile']))
